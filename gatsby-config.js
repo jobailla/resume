8d5d886637922426fe799d40149fbd8651781eb7
jobailla <jbaillais77@gmail.com>
2021-03-15 16:41:50 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    name: `Jonathan Baillais`,
+    tagline: `developper resume`,
+    title: `Jonathan Baillais resume`,
+  },  
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { graphql } from 'gatsby';
+import * as React from 'react';
+import * as styles from './Index.module.scss';
+
+interface IndexPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        name: string;
+        tagline: string;
+        title: string;
+      },
+    },
+  };
+}
+
+export const indexPageQuery = graphql`
+  query IndexPageQuery {
+    site {
+      siteMetadata {
+        name
+        tagline
+        title
+      }
+    }
+  }
+`;
+
+export default class IndexPage extends React.Component<IndexPageProps, {}> {
+
+  public render() {
+    const {
+      name,
+      tagline,
+      title
+    } = this.props.data.site.siteMetadata;
+
+    return (
+      <div className={styles.Container}>
+        <h1>{name}</h1>
+        <p>{tagline}</p>
+        <p>{title}</p>
+      </div>
+    );
+  }
+}
