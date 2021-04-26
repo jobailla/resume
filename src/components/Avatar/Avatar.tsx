52ec35e1059c8db08609d072817625e246540945
jobailla <jbaillais77@gmail.com>
2021-04-26 18:29:12 +0200

    avatar style

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 
-export default function Avatar(props: { url?: string; altText: string; title: string}): React.ReactElement {
-    const data = useStaticQuery(graphql`
+export default function Avatar(props: { url?: string; altText: string; title: string }): React.ReactElement {
+  const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "avatar.jpg" }) {
         childImageSharp {
@@ -15,23 +15,24 @@ export default function Avatar(props: { url?: string; altText: string; title: st
     }
   `);
 
-    const { url, altText, title } = props;
-    const style = {
-      width: "200px",
-      height: "200px",
-      borderRadius: "50%",
-    };
+  const { url, altText, title } = props;
+  const style = {
+    width: "200px",
+    height: "200px",
+    borderRadius: "50%",
+  };
 
-    if (url) {
-        return <img style={style} src={url} alt={altText} title={title} />;
-    }
+  if (url) {
+    return <img style={style} src={url} alt={altText} title={title} />;
+  }
 
-    return (
-        <Img
-            style={style}
-            fluid={data.placeholderImage.childImageSharp.fluid}
-            alt={altText}
-            title={title}
-        />
-    );
+  return (
+    <Img
+      style={style}
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      alt={altText}
+      title={title}
+      backgroundColor={'#1E5C83'}
+    />
+  );
 }
