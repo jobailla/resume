4062c84f427156187693f82e7d019d297d5cd8c0
jobailla <jbaillais77@gmail.com>
2021-04-29 16:18:13 +0200

    gatsby-plugin-fontawesome-css

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-fontawesome-css`,
     // SCSS
     `gatsby-plugin-sass`,
     // Typescript
